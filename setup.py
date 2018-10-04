09f35acd2aff462c57a71b190bfc528d9ab2f051
Tyson Boer <ty@unutilized.net>
2018-10-04 12:22:12 -0400

    Prep for 0.1.1 release

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ def readme():
 
 setuptools.setup(
     name='atiiaftt',
-    version='0.1',
+    version='0.1.1',
     description="""CFFI wrap of ATI-IA force-torque sensor transform c library""",
     long_description=readme(),
     long_description_content_type="text/x-rst",
     url='https://github.com/shayden--/atiiaftt',
-    download_url='https://github.com/shayden--/atiiaftt/tarball/0.1',
+    download_url='https://github.com/shayden--/atiiaftt/tarball/0.1.1',
     author='Tyson Boer',
     author_email='ty@unutilized.net',
     license='MIT',
